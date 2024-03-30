1ccd08b3acaca12a243f15d3f76a3c3a4bd37ff3
Shahzaib <shahzaibafzalofficial@gmail.com>
2024-03-30 23:09:50 +0500

    design footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-gray-100 px-10 py-4 text-center">
+      <p>&copy; 2024 Shahzaib ❤😊. All rights reserved.</p>
+    </div>
+  );
+};
+
+export default Footer;
